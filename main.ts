b2ef7337c6c16b7531812ce781d9b662f6d4ef6f
Flayaaa <Flayaaa@outlook.com>
2025-02-17 18:01:38 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,31 @@
+let Běží = false
+let dobaBěhu = 0
 
+radio.setGroup(10)
+
+input.onButtonPressed(Button.A, function () {
+    Sensors.SetLightLevel()
+    Běží = false
+})
+
+Sensors.OnLightDrop(function () {
+    if (Běží === false) {
+        music.playTone(Note.C, 500)
+        radio.sendNumber(53)
+        Běží = true
+    }
+
+})
+
+radio.onReceivedNumber(function (přijatéČíslo: number) {
+    music.playTone(Note.C, 500)
+    basic.showNumber(přijatéČíslo)
+    dobaBěhu = přijatéČíslo
+    basic.showNumber(dobaBěhu)
+})
+
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    Sensors.SetLightLevel()
+    Běží = false
+})
